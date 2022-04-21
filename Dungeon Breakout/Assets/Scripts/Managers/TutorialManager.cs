2d635e6f02e751f6524b3a076e2f051f6a57de82
Campinator <joefakocamp@gmail.com>
2022-04-21 17:34:29 -0500

    Starting on tutorial

---
+++ Dungeon Breakout/Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,31 @@
+/**** 
+ * Created by: Camp Steiner
+ * Date Created: 20 April, 2022
+ * 
+ * Last Edited by: Camp Steiner
+ * Last Edited: 20 April, 2022
+ * 
+ * Description: Tutorial to teach the player controls and basic battle
+ * 
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    private int step = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
